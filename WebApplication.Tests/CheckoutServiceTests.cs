bc9abe83c6a944f14584d159b2ecf7c24ca2369e
Shamithamr <129770006+Shamithamr@users.noreply.github.com>
2023-05-29 23:08:44 +0530

    Add files via upload

---
+++ WebApplication.Tests/CheckoutServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication4.Controllers;
+
+namespace WebApplication.Tests
+{
+    public class CheckoutServiceTests
+    {
+        [Fact]
+        public void CalculateTotalPrice_ReturnsCorrectTotalPrice()
+        {
+            // Arrange
+            var checkoutService = new CheckoutService();
+            string skus = "AAABBBCCDD";
+
+            // Act
+            decimal totalPrice = checkoutService.CalculateTotalPrice(skus);
+
+            // Assert
+            decimal expectedTotalPrice = 260;  // Total price for the given skus
+            Assert.Equal(expectedTotalPrice, totalPrice);
+        }
+    }
+}
